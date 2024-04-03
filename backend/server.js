52f8488fc7b157e9aec407c556efe03874ac66a8
unknown <imamovic.kerim@gmail.com>
2024-04-03 14:36:58 +0200

    kreirana MongoDB i postavke Mongoose - section 4
    -u folderu backend kreirani folderi: config, controllers, data, middleware, models, routes i js fajlovi u njima
    -instalirani paketu u terminalu: Bcrypt, Colors, Mongoose
    -kreiran seeder.js fajl
    -kreiran example.env fajl
    -potrebno kreirati .env fajl
    -manje izmjene u server.js fajlu
    -kreirane i testirane rute u Postman-u
    -manje izmjene u documentation.txt fajlu

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const PORT = process.env.PORT || 5000;
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
@@ -11,13 +15,9 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((prod) => prod._id === req.params.id);
-    res.json(product);
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
